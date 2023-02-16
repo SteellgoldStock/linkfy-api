5aabd4be47bb981e432d2164626a61de477c9708
Gaëtan H <steellgold0@gmail.com>
2023-02-16 19:35:25 +0100

    ✈️ Change host

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,9 @@ async function server() : Promise<void> {
   import("./routes/link/get/single");
   import("./routes/link/get/multiple");
 
-  app.listen({ port: 3000, host: "localhost" }, err => {
+  app.listen({ port: 3000 }, err => {
     if (err) throw err;
-    console.log("Server listening at http://localhost:3000");
+    console.log("Server listening at http://0.0.0.0:3000");
   });
 }
 
