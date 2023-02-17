8a3acffb82d4759808d1982071c0f7b88acebbfc
Gaëtan H <steellgold0@gmail.com>
2023-02-17 01:37:53 +0100

    🛰️ Host

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ async function server() : Promise<void> {
   import("./routes/link/get/single");
   import("./routes/link/get/multiple");
 
-  app.listen({ port: 3000 }, err => {
+  app.listen({ port: 3000, host: "0.0.0.0" }, err => {
     if (err) throw err;
     console.log("Server listening at http://0.0.0.0:3000");
   });
