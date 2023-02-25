40b90e6249727c493c223e2a51e209de3c45b53b
Gaëtan H <steellgold0@gmail.com>
2023-02-25 22:41:41 +0100

    🪄 `create` done();

--- src/routes/link/create/single.ts
+++ src/routes/link/create/single.ts
@@ -13,45 +13,34 @@ type BodyRequest = {
 }
 
 app.post("/link/create", {
-  // String because, when i sent a request, the body is a string and not an object (JSON.stringify())
   handler: async(request: FastifyRequest<{ Body: string }>, reply: FastifyReply) => {
-    // Body is "{"url":"https://google.com","slug":"google","password":"password","userId":"1","visitorId":"1"}"
-    // Convert body to object JSON, but JSON.parse() is not working
     const body: BodyRequest = JSON.parse(request.body);
-
     const schema = z.object({
       url: z.string().url(),
       slug: z.string().min(2).max(50),
-      password: z.string().default("none"),
-      userId: z.string().optional(),
+      password: z.string().nullable(),
+      userId: z.string().nullable(),
       visitorId: z.string()
     }).safeParse(body);
 
-    if (!schema.success) {
-      return reply.status(400).send({ error: "Invalid body" });
-    }
+    if (!schema.success) return reply.status(400).send({ error: "Invalid body" });
 
     const { url, slug, password, userId, visitorId } = schema.data;
 
-    let passwordHash = "";
-    if (password !== "none") {
-      passwordHash = await argon2d.hash(password);
-    }
+    let passwordHash = "none";
+    if (password !== null) passwordHash = await argon2d.hash(password);
 
     const link = await prisma.link.create({
       data: {
-        url: schema.data.url,
-        slug: schema.data.slug,
+        url: url,
+        slug: slug,
         password: passwordHash,
-        userId: schema.data.userId,
-        visitorId: schema.data.visitorId
+        userId: userId,
+        visitorId: visitorId
       }
     });
 
-    if (!link) {
-      return reply.status(500).send({ error: "Internal server error" });
-    }
-
+    if (!link) return reply.status(500).send({ error: "Internal server error" });
     return reply.status(200).send(link);
   }
 });
