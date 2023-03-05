601f7d1f23a8effb0d7015f4a802f919cadf8a35
Gaëtan H <steellgold0@gmail.com>
2023-03-05 18:14:33 +0100

    😗 Todo refactor

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,12 @@ export const app = fastify();
 
 async function server() : Promise<void> {
   await app.register(cors, {
-    origin: "*" // TODO: Change this to a whitelist of domains
-    // THIS IS NOT SECURE: ONLY USE FOR DEVELOPMENT
+    origin: "*"
   });
-
-  import("./routes/link/create/single");
-  import("./routes/link/get/single");
-  import("./routes/link/get/multiple");
-  import("./routes/link/put/increment");
-
-  import("./routes/link/password/verify");
 
   app.listen({ port: 3000, host: "0.0.0.0" }, err => {
     if (err) throw err;
-    console.log("Server listening at http://0.0.0.0:3000");
+    console.info("Server listening at http://0.0.0.0:3000");
   });
 }
 
--- src/routes/link/create/single.ts
+++ src/routes/link/create/single.ts
@@ -1,46 +1,9 @@
 import * as argon2d from "argon2";
 import { FastifyReply, FastifyRequest } from "fastify";
-import { z } from "zod";
 import { app } from "../../..";
-import prisma from "../../../utils/prisma";
-
-type BodyRequest = {
-  url: string,
-  slug: string,
-  password: string,
-  userId?: string,
-  visitorId: string
-}
 
 app.post("/link/create", {
   handler: async(request: FastifyRequest<{ Body: string }>, reply: FastifyReply) => {
-    const body: BodyRequest = JSON.parse(request.body);
-    const schema = z.object({
-      url: z.string().url(),
-      slug: z.string().min(2).max(50),
-      password: z.string().nullable(),
-      userId: z.string().nullable(),
-      visitorId: z.string()
-    }).safeParse(body);
-
-    if (!schema.success) return reply.status(400).send({ error: "Invalid body" });
-
-    const { url, slug, password, userId, visitorId } = schema.data;
-
-    let passwordHash = "none";
-    if (password !== null) passwordHash = await argon2d.hash(password);
-
-    const link = await prisma.link.create({
-      data: {
-        url: url,
-        slug: slug,
-        password: passwordHash,
-        userId: userId,
-        visitorId: visitorId
-      }
-    });
-
-    if (!link) return reply.status(500).send({ error: "Internal server error" });
-    return reply.status(200).send(link);
+    return reply.send({ message: "Hello World!" });
   }
 });
